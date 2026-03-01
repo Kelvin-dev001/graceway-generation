646b69cec77b77d85687bbb213c6c66bca5074b5
Kelvin-dev001 <kelvinoyugi101@gmail.com>
2026-03-01 14:37:04 +0300

    navbar arrange

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,20 @@
+import Navbar from "@/components/Navbar";
 import HeroSection from "@/components/HeroSection";
 import ProblemSection from "@/components/ProblemSection";
 import VisionSection from "@/components/VisionSection";
 import BenefitsSection from "@/components/BenefitsSection";
 import FinalCTASection from "@/components/FinalCTASection";
-import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
 export default function HomePage() {
   return (
     <main className="min-h-screen">
+      <Navbar />
       <HeroSection />
       <ProblemSection />
       <VisionSection />
       <BenefitsSection />
       <FinalCTASection />
-      <Navbar />
       <Footer />
     </main>
   );
